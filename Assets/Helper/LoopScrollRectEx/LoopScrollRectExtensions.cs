1647fe1bedd8b4af92db065120439309edaad6da
nitou-kanazawa <121334451+nitou-kanazawa@users.noreply.github.com>
2025-01-16 23:15:49 +0900

    LoopScrollRectの拡張メソッド

--- Assets/Helper/LoopScrollRectEx/LoopScrollRectExtensions.cs
+++ Assets/Helper/LoopScrollRectEx/LoopScrollRectExtensions.cs
@@ -3,8 +3,8 @@ using UniRx;
 
 // [REF]
 //  github: 
-//  
 //  _: 無限スクロールビューライブラリ 二つの設計思想比較 https://speakerdeck.com/harumak/wu-xian-sukurorubiyuraiburari-er-tufalseshe-ji-si-xiang-bi-jiao?slide=4
+//  qiita: LoopScrollRect をスナップさせる試み https://qiita.com/Yamara/items/f426562b5530e08ee67a
 
 namespace UnityEngine.UI {
 
@@ -20,6 +20,14 @@ namespace UnityEngine.UI {
         /// <summary>
         /// 
         /// </summary>
+        /// <param name="scrollRect"></param>
+        public static IObservable<Vector2> OnValueChangeAsObservable(this LoopScrollRect scrollRect) {
+            return scrollRect.onValueChanged.AsObservable();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public static IDisposable RegisterDataSource<T>(this LoopScrollRect scrollRect,
             int count, Action<T, int> provideData)
             where T : Component {
@@ -31,8 +39,8 @@ namespace UnityEngine.UI {
             scrollRect.RefillCells();
 
             // Disposable
-            return Disposable.Create(() => { 
-                if(scrollRect.dataSource == dataSource) {
+            return Disposable.Create(() => {
+                if (scrollRect.dataSource == dataSource) {
                     scrollRect.dataSource = null;
                     scrollRect.totalCount = 0;
                     scrollRect.ClearCells();
@@ -40,17 +48,6 @@ namespace UnityEngine.UI {
                 dataSource.Dispose();
             });
         }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="scrollRect"></param>
-        public static IObservable<Vector2> OnValueChangeAsObservable(this LoopScrollRect scrollRect) {
-            return scrollRect.onValueChanged.AsObservable();
-        } 
-
-
-
 
         private class DataSource<T> : LoopScrollDataSource
             where T : Component {
@@ -66,6 +63,20 @@ namespace UnityEngine.UI {
                 throw new NotImplementedException();
             }
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void Snap(this LoopScrollRectBase scrollRect, 
+            float width, int defaultViewCount = int.MaxValue, float duration = 0.2f) {
+            
+            scrollRect.StopMovement();
+            if (scrollRect.content.childCount <= defaultViewCount) return;
+
+            var firstIndex = scrollRect.GetFirstItem(out var offset);
+            scrollRect.ScrollToCellWithinTime(firstIndex + (offset >= -width * 0.5f ? 1 : 0), duration);
+        }
+
     }
 
 }
